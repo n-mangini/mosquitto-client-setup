4d9f205d975b3d0e4beee0757f91371d9ea7e692
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-11-01 17:14:00 -0300

    indent

--- python-client/client.py
+++ python-client/client.py
@@ -1,13 +1,11 @@
 import paho.mqtt.client as mqtt
 from include.config import CERT,BROKER, PORT, USERNAME, PASSWORD
 
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("test/topic")
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
-
 
 client = mqtt.Client()
 client.username_pw_set(USERNAME, PASSWORD)
